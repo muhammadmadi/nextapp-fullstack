8734d483eda2a3d4b1b0008d4e1c8efed81fe595
muhammadmadi <muhammadmadi1990@gmail.com>
2023-10-25 18:48:03 +0300

    put DELETE

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,14 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export function GET(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User no found" }, { status: 404 });
 
-export function GET(request : NextRequest , {params} : {params : {id:number}}) {
+  return NextResponse.json([
+    { id: 1, name: "mosh" },
+    { id: 2, name: "john" },
+  ]);
+}
 
-if(params.id >10  )
-return   NextResponse.json({error:'User no found'}, {status : 404})
-    
-return  NextResponse.json([
-    {id: 1, "name": "mosh"},
-    {id: 2, "name": "john"}
-])
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  //check if use name invalid
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required " }, { status: 400 });
+//check if user exist
+ if (params.id > 10)
+    return NextResponse.json({ error: "User no found" }, { status: 404 });
 
-} 
+    return NextResponse.json({id:1 , name :"moe"}  );
+}
+
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+   const body = await request.json();
+//check if user exist
+ if (params.id > 10)
+    return NextResponse.json({ error: "User no found" }, { status: 404 });
+
+    return NextResponse.json('user deleted ' , {status : 200} ); 
+}
