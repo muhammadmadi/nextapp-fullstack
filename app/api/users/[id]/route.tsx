9abccc785ad32db46c27e9f0fd3c9ad95b6e81cd
muhammadmadi <muhammadmadi1990@gmail.com>
2023-10-26 00:31:42 +0300

    users = await prisma.user.findMany()

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,17 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
-export function GET(
+export async function GET(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
-  if (params.id > 10)
-    return NextResponse.json({ error: "User no found" }, { status: 404 });
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
 
-  return NextResponse.json([
-    { id: 1, name: "mosh" },
-    { id: 2, name: "john" },
-  ]);
+  return NextResponse.json(user);
 }
 
 export async function PUT(
@@ -23,22 +25,21 @@ export async function PUT(
   //check if use name invalid
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-//check if user exist
- if (params.id > 10)
+  //check if user exist
+  if (params.id > 10)
     return NextResponse.json({ error: "User no found" }, { status: 404 });
 
-    return NextResponse.json({id:1 , name :"moe"}  );
+  return NextResponse.json({ id: 1, name: "moe" });
 }
-
 
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-   const body = await request.json();
-//check if user exist
- if (params.id > 10)
+  const body = await request.json();
+  //check if user exist
+  if (params.id > 10)
     return NextResponse.json({ error: "User no found" }, { status: 404 });
 
-    return NextResponse.json('user deleted ' , {status : 200} ); 
+  return NextResponse.json("user deleted ", { status: 200 });
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  let asd = NextResponse.json([
-    { id: 1, name: "jane" },
-    { id: 2, name: "john" },
-  ]);
 
-  return asd;
+
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany()
+
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
