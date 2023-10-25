ff4099c392a4b3958a827c1deecc22e03a5d3013
muhammadmadi <muhammadmadi1990@gmail.com>
2023-10-25 18:08:39 +0300

    export async function POST

---
+++ app/api/users/route.tsx
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest) {
+  let asd = NextResponse.json([
+    { id: 1, name: "jane" },
+    { id: 2, name: "john" },
+  ]);
+
+  return asd;
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required " }, { status: 400 });
+  return NextResponse.json(
+    {
+      id: 1,
+      name: body.name,
+    },
+    { status: 201 }
+  );
+}
