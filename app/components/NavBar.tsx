2a2c4506836765438cf647ccac4e37470cc5ec54
muhammadmadi <muhammadmadi1990@gmail.com>
2023-10-27 21:10:46 +0300

    npm install next-auth

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,12 +3,15 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex p-6">
-      <Link href="/" className="mr-5 btn btn-primary">
+    <div className="flex p-6 bg-slate-400  space-x-3 ">
+      <Link href="/" className=" btn btn-primary">
         Home Page
       </Link>
-      <Link href='/users' className="mr-5 btn btn-primary"> Users Page </Link>
-       <Link href='/upload' className="mr-5 btn btn-primary"> Upload Page  </Link>
+      <Link href='/users' className=" btn btn-primary"> Users Page </Link>
+       <Link href='/upload' className=" btn btn-primary"> Upload Page  </Link>
+
+<Link href='/api/auth/signin' className=" btn btn-primary"> Login  </Link>
+
     </div>
   );
 };
