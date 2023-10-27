57ccffa55254fac48fa0d3dc9150f0ec9215fde4
muhammadmadi <muhammadmadi1990@gmail.com>
2023-10-27 16:46:06 +0300

    Showing Uploaded Images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,14 +1,33 @@
-'use client';
-import React from 'react'
-import { CldUploadWidget } from 'next-cloudinary'
+"use client";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+interface CloudinaryResult {
+  public_id: string;
+}
+
 
 
 const UploadPage = () => {
+  const [PublicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset='jaodqemz'>  
-         {({open})=> <button className='btn btn-primary' onClick={()=> open()}> Upload  </button>}
-    </CldUploadWidget>
-  )
-}
+    <>
+      {PublicId && <CldImage  src="PublicId" width={270} height={180} alt=' a coffee image ' /> }
+      <CldUploadWidget
+        uploadPreset="jaodqemz"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            {" "}
+            Upload{" "}
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
 
-export default UploadPage
+export default UploadPage;
