0e4f7d64e5239ef0bc2fa00bf648a9235c16e2b9
muhammadmadi <muhammadmadi1990@gmail.com>
2023-10-27 15:28:51 +0300

    CldUploadWidget uploadPreset

---
+++ app/upload/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react'
+import { CldUploadWidget } from 'next-cloudinary'
+
+
+const UploadPage = () => {
+  return (
+    <CldUploadWidget uploadPreset='jaodqemz'>  
+         {({open})=> <button className='btn btn-primary' onClick={()=> open()}> Upload  </button>}
+    </CldUploadWidget>
+  )
+}
+
+export default UploadPage
