c1bc083438c9130c93796941492e1bcea957bd65
muhammadmadi <muhammadmadi1990@gmail.com>
2023-10-27 22:53:07 +0300

    <AuthProvider>

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,15 @@
+'use client'
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const {status ,data: Session } = useSession();
+  //  if (status === 'loading') return null;
+
   return (
-    <div className="flex p-6 bg-slate-400  space-x-3 ">
+    <div className=" navbar flex p-6 bg-slate-400  space-x-3   ">
+ 
       <Link href="/" className=" btn btn-primary">
         Home Page
       </Link>
@@ -11,7 +17,9 @@ const NavBar = () => {
        <Link href='/upload' className=" btn btn-primary"> Upload Page  </Link>
 
 <Link href='/api/auth/signin' className=" btn btn-primary"> Login  </Link>
-
+    {status === 'loading'&& <div className="loading loading-bars loading-lg" > </div> }
+   {status ==='authenticated' && <div> {Session.user!.name}</div>  }
+   {status === 'unauthenticated'&& <Link href='/api/auth/signin'> </Link>}
     </div>
   );
 };
